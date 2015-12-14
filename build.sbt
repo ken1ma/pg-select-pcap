5542e1d7175f714ffaafdf0758ae8f1e777a1dc3
Kenichi Masuko <kenichi@sysart.jp>
2015-12-14 11:10:14 +0900

    test prototype

--- build.sbt
+++ build.sbt
@@ -7,6 +7,10 @@ lazy val root = (project in file(".")).
       "org.pcap4j" % "pcap4j-core" % "1.6.1",
       "org.pcap4j" % "pcap4j-packetfactory-static" % "1.6.1",
       //"org.pcap4j" % "pcap4j-packetfactory-propertiesbased" % "1.6.1",
-      "ch.qos.logback" % "logback-classic" % "1.1.3"
-    )
+      "ch.qos.logback" % "logback-classic" % "1.1.3",
+	  "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test",
+	  "org.postgresql" % "postgresql" % "9.4-1206-jdbc42" % "test"
+    ),
+	fork := true, // avoid No suitable driver found
+	test in assembly := {}
   )
---
+++ src/test/scala/jp.ken1ma.pg_select_pcap/NoParamsTest.scala
@@ -0,0 +1,67 @@
+package jp.ken1ma.pg_select_pcap
+
+import java.util.Properties
+import java.sql._
+
+import org.scalatest.FunSuite
+
+class NoParamsTest extends FunSuite {
+	val host = "192.168.1.11"
+	val db = "pcap_test_db"
+	val user = "pcap_test_user"
+	val password = "pcap_test_user"
+
+	var con: Connection = _
+
+	override def withFixture(test: NoArgTest) = {
+		val props = new Properties
+		props.setProperty("user", user);
+		props.setProperty("password", password);
+		con = DriverManager.getConnection(s"jdbc:postgresql://$host/$db", props)
+		def execute(sql: String) {
+			val s = con.createStatement
+			try {
+				s.execute(sql)
+			} finally {
+				s.close
+			}
+		}
+		try {
+			execute("CREATE TABLE t0(id integer, name text)")
+			super.withFixture(test)
+		} finally {
+			execute("DROP TABLE T0");
+			con.close
+		}
+	}
+
+/*
+	test("Statement.execute") {
+		val s = con.createStatement
+		try {
+			s.execute("select * from T0")
+		} finally {
+			s.close
+		}
+	}
+
+	test("PreparedStatement.executeQuery") {
+		val ps = con.prepareStatement("select * from T0")
+		try {
+			ps.executeQuery
+		} finally {
+			ps.close
+		}
+	}
+*/
+
+	test("PreparedStatement.executeQuery with parameters") {
+		val ps = con.prepareStatement("select * from T0 where name = ?")
+		ps.setString(1, "foo")
+		try {
+			ps.executeQuery
+		} finally {
+			ps.close
+		}
+	}
+}
